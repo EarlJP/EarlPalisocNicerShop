747c667175df03087e47afc2ebe5af602c31ab34
Earl <earlj_pal@hotmail.ca>
2019-10-13 17:00:38 -0400

    Commit, Added model, instantiate object

--- app/src/main/java/com/earljaepal/palisocnicershop/MenuActivity.java
+++ app/src/main/java/com/earljaepal/palisocnicershop/MenuActivity.java
@@ -7,12 +7,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.earljaepal.palisocnicershop.model.ItemShop;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MenuActivity extends AppCompatActivity {
     private static final String LOG_TAG =
             MenuActivity.class.getSimpleName();
 
     public static final String EXTRA_MESSAGE =
             "com.earljaepal.palisocshop.extra.MESSAGE";
+
+    private List mItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +36,51 @@ public class MenuActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
+
+        mItems = new ArrayList<ItemShop>();
+        ItemShop item1 = new ItemShop(getResources().getString(R.string.item1_name),
+                R.drawable.mach_bike,
+                getResources().getString(R.string.item1_price),
+                getResources().getString(R.string.item1_description),
+                getResources().getString(R.string.quantity),
+                getResources().getString(R.string.remove_button),
+                getResources().getString(R.string.initial_quantity),
+                getResources().getString(R.string.add_button),
+                getResources().getString(R.string.subtotal_title),
+                getResources().getString(R.string.initial_subtotal)
+                );
+
+        mItems.add(item1);
+
+        ItemShop item2 = new ItemShop(getResources().getString(R.string.item2_name),
+                R.drawable.acro_bike,
+                getResources().getString(R.string.item2_price),
+                getResources().getString(R.string.item2_description),
+                getResources().getString(R.string.quantity),
+                getResources().getString(R.string.remove_button),
+                getResources().getString(R.string.initial_quantity),
+                getResources().getString(R.string.add_button),
+                getResources().getString(R.string.subtotal_title),
+                getResources().getString(R.string.initial_subtotal)
+        );
+
+        mItems.add(item2);
+
+        ItemShop item3 = new ItemShop(getResources().getString(R.string.item3_name),
+                R.drawable.acro_bike,
+                getResources().getString(R.string.item3_price),
+                getResources().getString(R.string.item3_description),
+                getResources().getString(R.string.quantity),
+                getResources().getString(R.string.remove_button),
+                getResources().getString(R.string.initial_quantity),
+                getResources().getString(R.string.add_button),
+                getResources().getString(R.string.subtotal_title),
+                getResources().getString(R.string.initial_subtotal)
+        );
+
+        mItems.add(item3);
     }
 
 }
---
+++ app/src/main/java/com/earljaepal/palisocnicershop/model/ItemShop.java
@@ -0,0 +1,114 @@
+package com.earljaepal.palisocnicershop.model;
+
+import android.graphics.drawable.Drawable;
+import android.widget.Button;
+
+public class ItemShop {
+    private String mItemTitle;
+    private int mItemImage;
+    private String mItemPrice;
+    private String mItemDescription;
+
+    private String mQuantityTitle;
+    private String mRemoveButton;
+    private String mItemQuantity;
+    private String mAddButton;
+    private String mSubtotalTitle;
+    private String mInitialSubtotal;
+
+    public ItemShop(String mItemTitle, int mItemImage, String mItemPrice, String mItemDescription,
+                    String mQuantityTitle, String mRemoveButton, String mItemQuantity,
+                    String mAddButton, String mSubtotalTitle, String mInitialSubtotal) {
+        this.mItemTitle = mItemTitle;
+        this.mItemImage = mItemImage;
+        this.mItemPrice = mItemPrice;
+        this.mItemDescription = mItemDescription;
+        this.mQuantityTitle = mQuantityTitle;
+        this.mRemoveButton = mRemoveButton;
+        this.mItemQuantity = mItemQuantity;
+        this.mAddButton = mAddButton;
+        this.mSubtotalTitle = mSubtotalTitle;
+        this.mInitialSubtotal = mInitialSubtotal;
+    }
+
+
+    public String getmItemTitle() {
+        return mItemTitle;
+    }
+
+    public void setmItemTitle(String mItemTitle) {
+        this.mItemTitle = mItemTitle;
+    }
+
+    public int getmItemImage() {
+        return mItemImage;
+    }
+
+    public void setmItemImage(int mItemImage) {
+        this.mItemImage = mItemImage;
+    }
+
+    public String getmItemPrice() {
+        return mItemPrice;
+    }
+
+    public void setmItemPrice(String mItemPrice) {
+        this.mItemPrice = mItemPrice;
+    }
+
+    public String getmItemDescription() {
+        return mItemDescription;
+    }
+
+    public void setmItemDescription(String mItemDescription) {
+        this.mItemDescription = mItemDescription;
+    }
+
+    public String getmQuantityTitle() {
+        return mQuantityTitle;
+    }
+
+    public void setmQuantityTitle(String mQuantityTitle) {
+        this.mQuantityTitle = mQuantityTitle;
+    }
+
+    public String getmRemoveButton() {
+        return mRemoveButton;
+    }
+
+    public void setmRemoveButton(String mRemoveButton) {
+        this.mRemoveButton = mRemoveButton;
+    }
+
+    public String getmItemQuantity() {
+        return mItemQuantity;
+    }
+
+    public void setmItemQuantity(String mItemQuantity) {
+        this.mItemQuantity = mItemQuantity;
+    }
+
+    public String getmAddButton() {
+        return mAddButton;
+    }
+
+    public void setmAddButton(String mAddButton) {
+        this.mAddButton = mAddButton;
+    }
+
+    public String getmSubtotalTitle() {
+        return mSubtotalTitle;
+    }
+
+    public void setmSubtotalTitle(String mSubtotalTitle) {
+        this.mSubtotalTitle = mSubtotalTitle;
+    }
+
+    public String getmInitialSubtotal() {
+        return mInitialSubtotal;
+    }
+
+    public void setmInitialSubtotal(String mInitialSubtotal) {
+        this.mInitialSubtotal = mInitialSubtotal;
+    }
+}
